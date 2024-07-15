8ee760902dcc8e8377449849284a448739aff264
flooijdt <flooijdt@posteo.de>
2024-07-15 18:14:48 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let c: CaesarCipher = CaesarCipher::new(5);
     println!("{}", c.encode("Codewars"));
+    println!("{}", c.decode("HTIJBFWX"));
+    println!("{}", c.decode("IT'S A SHIFT CIPHER!!"));
+    println!("{}", 'Z' as u8);
+    println!("{}", 'A' as u8);
 }
 
 struct CaesarCipher {
@@ -15,14 +19,32 @@ impl CaesarCipher {
     fn encode(&self, message: &str) -> String {
         let messageu = message.to_uppercase();
 
-        let vecky: String = messageu
+        let mut vecky2: Vec<u8> = messageu
             .chars()
-            .map(|x| (x as u8 + self.shift as u8) as char)
+            .map(|x| (x as u8 + self.shift as u8))
             .collect();
-        vecky
+
+        for i in vecky2.iter_mut() {
+            if *i > 90 {
+                *i = 64 + *i - 90;
+            }
+        }
+        vecky2.iter().map(|x| *x as char).collect()
     }
 
     fn decode(&self, message: &str) -> String {
-        todo!();
+        let messageu = message.to_uppercase();
+
+        let mut vecky2: Vec<u8> = messageu
+            .chars()
+            .map(|x| (x as u8 - self.shift as u8))
+            .collect();
+
+        for i in vecky2.iter_mut() {
+            if *i < 65 {
+                *i = *i + 90 - 64;
+            }
+        }
+        vecky2.iter().map(|x| *x as char).collect()
     }
 }
