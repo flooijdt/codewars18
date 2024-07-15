4bb10a2671959dc082ba7db9a73ed0614cb87827
flooijdt <flooijdt@posteo.de>
2024-07-15 16:20:53 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let c: CaesarCipher = CaesarCipher::new(5);
     c.encode("mess");
-    println!("Hello, world!");
 }
 
 struct CaesarCipher {
@@ -15,8 +14,13 @@ impl CaesarCipher {
 
     fn encode(&self, message: &str) -> String {
         let mut vecky: Vec<char> = Vec::new();
-        vecky = message.chars().collect();
-        println!("{:#?}", vecky);
+
+        let messageu = message.to_uppercase();
+
+        vecky = messageu.chars().collect();
+
+        println!("{:x}", vecky[0] as u32);
+        println!("{}", String::from("\u{004D}"));
         "string".to_string()
     }
 
