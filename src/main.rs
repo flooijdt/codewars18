f319b2d8e41ef150321beee40b15716046a031b5
flooijdt <flooijdt@posteo.de>
2024-07-15 14:59:59 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct CaesarCipher {
+    shift: u32,
+}
+
+impl CaesarCipher {
+    fn new(shift: u32) -> CaesarCipher {
+        CaesarCipher { shift }
+    }
+
+    fn encode(&self, message: &str) -> String {
+        todo!();
+    }
+
+    fn decode(&self, message: &str) -> String {
+        todo!();
+    }
+}
